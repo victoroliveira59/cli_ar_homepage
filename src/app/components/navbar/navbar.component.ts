3bd0d7060b75a6247baa565b03fe65dd1082bee0
Victor Oliveira <victor.oliveira@rumosolucoes.com>
2025-02-11 13:17:41 -0300

    fix: voltado a versão antiga para corrigir bug

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,61 +1,36 @@
 import { Component, HostListener } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { RouterModule } from '@angular/router'
 import { ScrollService } from '../../scroll.service'
 
 @Component({
   selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  menuAberto = false  // Estado do menu (aberto/fechado)
-  menuVisible = true  // Controla visibilidade do menu com rolagem
+  menuAberto = false
+  menuVisible = true
 
   constructor(private scrollService: ScrollService) {}
 
-  // Alterna a abertura e fechamento do menu
+  // Alternar abertura/fechamento do sidebar
   toggleMenu() {
     this.menuAberto = !this.menuAberto
-    this.toggleOverlay(this.menuAberto)
   }
 
-  // Fecha o menu e o overlay quando um link é clicado
+  // Rolagem suave para a seção correspondente
   scrollToSection(event: Event, sectionId: string): void {
     event.preventDefault()
     this.scrollService.scrollToSection(sectionId)
-    this.menuAberto = false
-    this.toggleOverlay(false)
+    this.menuAberto = false // Fecha o sidebar após clicar em um link
   }
 
-  // Exibe ou esconde o overlay
-  toggleOverlay(show: boolean) {
-    const overlay = document.querySelector('.overlay') as HTMLElement
-    if (overlay) {
-      if (show) {
-        overlay.classList.add('active')
-      } else {
-        overlay.classList.remove('active')
-      }
-    }
-  }
-
-  // Fecha o menu clicando fora do sidebar
-  closeMenuOnClickOutside(event: Event) {
-    const navLinks = document.querySelector('.nav-links') as HTMLElement
-    if (this.menuAberto && !navLinks.contains(event.target as Node)) {
-      this.menuAberto = false
-      this.toggleOverlay(false)
-    }
-  }
-
-  // Esconde o menu com a rolagem para baixo
-  @HostListener('window:scroll', ['$event'])
+  // Esconde o menu quando o usuário rolar para baixo
+  @HostListener('window:scroll', [])
   onWindowScroll() {
     this.menuVisible = window.scrollY <= 50
-  }
-
-  // Fecha o menu se clicar fora dele
-  @HostListener('document:click', ['$event'])
-  onDocumentClick(event: Event) {
-    this.closeMenuOnClickOutside(event)
   }
 }
