b6c2faf2204ef5a05b91c6e45afa7f2a4e454bb4
Victor Oliveira <victor.oliveira@rumosolucoes.com>
2025-02-11 12:38:57 -0300

    Ajuste no menu responsivo

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,42 +1,36 @@
-import { Component, HostListener } from '@angular/core';
-import { NavigationEnd, Router, RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { filter } from 'rxjs';
-import { ScrollService } from '../../scroll.service';
+import { Component, HostListener } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { RouterModule } from '@angular/router'
+import { ScrollService } from '../../scroll.service'
 
 @Component({
   selector: 'app-navbar',
+  standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  title = 'cli_ar';
-  isHomePage = true; // Inicialmente assume que é a home
-  menuAberto = false; // Inicialmente assume que o menu está fechado
-  menuVisible = true;  // Controla a visibilidade do menu
+  menuAberto = false
+  menuVisible = true
 
-  constructor(private scrollService: ScrollService) { }
+  constructor(private scrollService: ScrollService) {}
 
-  // Função para alternar a abertura e fechamento do menu
+  // Alternar abertura/fechamento do sidebar
   toggleMenu() {
-    this.menuAberto = !this.menuAberto;
+    this.menuAberto = !this.menuAberto
   }
 
-  // Usando o serviço para rolar para a seção
+  // Rolagem suave para a seção correspondente
   scrollToSection(event: Event, sectionId: string): void {
-    event.preventDefault(); // Impede o comportamento padrão de navegação
-    this.scrollService.scrollToSection(sectionId); // Chama o método do serviço
-    this.menuAberto = false; // Fecha o menu após o clique
+    event.preventDefault()
+    this.scrollService.scrollToSection(sectionId)
+    this.menuAberto = false // Fecha o sidebar após clicar em um link
   }
 
-  // Listener de rolagem para esconder o menu
-  @HostListener('window:scroll', ['$event'])
+  // Esconde o menu quando o usuário rolar para baixo
+  @HostListener('window:scroll', [])
   onWindowScroll() {
-    if (window.scrollY > 50) {  // Quando a rolagem passar de 50px, esconder o menu
-      this.menuVisible = false;
-    } else {
-      this.menuVisible = true;  // Quando o usuário voltar para o topo, mostrar o menu novamente
-    }
+    this.menuVisible = window.scrollY <= 50
   }
 }
