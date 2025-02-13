338b77bc21936f270653c8b491647014c0f13722
Victor Samuel Oliveira <victorsamueloliveira59@gmail.com>
2025-02-13 16:11:14 -0300

    fix: correção de bug de carregamento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,45 @@
-import { Component } from '@angular/core';
-import { Router, NavigationEnd, RouterModule } from '@angular/router';
-import { filter } from 'rxjs';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component, ElementRef, QueryList, ViewChildren, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { ServicosComponent } from "./pages/servicos/servicos.component";
+import { SobreComponent } from "./pages/sobre/sobre.component";
+import { ContatoComponent } from "./pages/contato/contato.component";
 import { FooterComponent } from "./components/footer/footer.component";
-import { HomeComponent } from './pages/home/home.component'
-import { ServicosComponent } from './pages/servicos/servicos.component'
-import { SobreComponent } from './pages/sobre/sobre.component'
-import { ContatoComponent } from './pages/contato/contato.component'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule,
-    RouterModule,
-    NavbarComponent,
-    FooterComponent,
-    HomeComponent,
-    ServicosComponent,
-    SobreComponent,
-    ContatoComponent],
-
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [NavbarComponent, HomeComponent, ServicosComponent, SobreComponent, ContatoComponent, FooterComponent, CommonModule]
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  visibleSections: { [key: string]: boolean } = {
+    home: true, // O "home" carrega imediatamente
+    servicos: false,
+    sobre: false,
+    contato: false
+  };
 
+  @ViewChildren('sectionRef') sections!: QueryList<ElementRef>;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId) && 'IntersectionObserver' in window) {
+      const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            const sectionId = entry.target.getAttribute('id');
+            if (sectionId) {
+              this.visibleSections[sectionId] = true;
+            }
+          }
+        });
+      }, { threshold: 0.2 }); // Ativa quando 20% da seção está visível
+
+      this.sections.forEach(section => observer.observe(section.nativeElement));
+    }
+  }
 }
