336eb7385f79e1a5bec214eacea273e5e48aad4b
Victor Samuel Oliveira <victorsamueloliveira59@gmail.com>
2025-02-20 17:03:48 -0300

    feta: criado componete de botão

---
+++ src/app/components/urgency-button/urgency-button.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  standalone: true,
+  selector: 'app-urgency-button',
+  templateUrl: './urgency-button.component.html',
+  styleUrls: ['./urgency-button.component.css'],
+  imports: [CommonModule]
+})
+export class UrgencyButtonComponent {
+  isExpanded = false;
+
+  toggleMessage() {
+    this.isExpanded = !this.isExpanded;
+  }
+}
