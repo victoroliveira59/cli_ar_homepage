737dfeedb72f75bac385d1c0bf8a1347f3be5d13
Victor Samuel Oliveira <victorsamueloliveira59@gmail.com>
2025-04-03 20:46:16 -0300

    corrigindo erro e alterando posição de logo

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
 import { provideNgxMask } from 'ngx-mask'; // Corrigido o import
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
-    provideNgxMask() // Adicionado corretamente
+    provideNgxMask(), // Adicionado corretamente
+    provideAnimations()
   ]
 }).catch((err) => console.error(err));
